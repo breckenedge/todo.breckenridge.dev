e4582c366654893cbfac3f3b6bf0cba811948866
Aaron Breckenridge <aaronbreckenridge@gmail.com>
2020-05-10 10:34:56 -0500

    Completion icons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def linkify_text(text)
+    text.split("\n").map do |line|
+      line.split(' ').map do |word|
+        if word.start_with?(%r{https?://})
+          link_to(word, word)
+        else
+          word
+        end
+      end.join(' ')
+    end.join("\n")
+  end
 end
--- app/helpers/icons_helper.rb
+++ app/helpers/icons_helper.rb
@@ -3,6 +3,10 @@ module IconsHelper
     '✓'
   end 
 
+  def complete_icon_for(item)
+    content_tag(:span, complete_icon, class: 'accent4') if item.complete?
+  end
+
   def incomplete_icon
     '×'
   end
