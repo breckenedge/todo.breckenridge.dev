5543e0fd34ba1fdea973e41f956232f7c82ab967
Aaron Breckenridge <aaronbreckenridge@gmail.com>
2020-05-13 17:25:34 -0500

    Rubocop: Layout/FirstArrayElementIndentation

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -1,12 +1,14 @@
 module NavigationHelper
   def navigation
-    safe_join([
-      link_to('Projects', projects_path, id: 'projects-link', class: ('active' if project_active?)),
-      link_to('Todos', todos_path, id: 'todos-link', class: ('active' if todos_active?)),
-      link_to('🔍', search_path, id: 'search-link', class: ('active' if search_active?)),
-      content_tag(:div, '', style: 'flex-grow: 1'),
-      link_to('⚙', profile_path, id: 'profile-link', class: ('active' if profile_active?))
-    ])
+    safe_join(
+      [
+        link_to('Projects', projects_path, id: 'projects-link', class: ('active' if project_active?)),
+        link_to('Todos', todos_path, id: 'todos-link', class: ('active' if todos_active?)),
+        link_to('🔍', search_path, id: 'search-link', class: ('active' if search_active?)),
+        content_tag(:div, '', style: 'flex-grow: 1'),
+        link_to('⚙', profile_path, id: 'profile-link', class: ('active' if profile_active?))
+      ]
+    )
   end
 
   def profile_active?
