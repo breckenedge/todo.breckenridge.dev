346d92b933cb1f4800477b4055423c6d17d7fc16
Aaron Breckenridge <aaronbreckenridge@gmail.com>
2020-05-13 11:18:52 -0500

    Automated deployments :fingerscrossed:

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 
+# This script runs on the docker server to deploy the application. It can be kicked off locally via:
+#
+# ```
+# ssh todo.breckenridge.dev "RAILS_MASTER_KEY=$(cat config/master.key) sh -s" < deploy.sh
+# ```
+
 set -e
 
+echo 'Pulling latest'
 docker pull docker.pkg.github.com/breckenedge/todo.breckenridge.dev/todo:latest
 
-docker run -it --rm -e RAILS_MASTER_KEY=$(cat config/master.key) -v "/var/lib/docker/volumes/todo_db/_data/production.sqlite3:/code/db/production.sqlite3" docker.pkg.github.com/breckenedge/todo.breckenridge.dev/todo:latest rails db:migrate
+echo 'Running migrations'
+docker run --rm -e RAILS_MASTER_KEY=$RAILS_MASTER_KEY -v "/var/lib/docker/volumes/todo_db/_data/production.sqlite3:/code/db/production.sqlite3" docker.pkg.github.com/breckenedge/todo.breckenridge.dev/todo:latest rails db:migrate
 
-docker container stop todo
+echo 'Stopping the container'
+docker container stop todo || true
 
-docker run --rm --name todo -d -p 3000:3000 -e RAILS_MASTER_KEY=$(cat config/master.key) -v "/var/lib/docker/volumes/todo_db/_data/production.sqlite3:/code/db/production.sqlite3" docker.pkg.github.com/breckenedge/todo.breckenridge.dev/todo:latest rails s
+echo 'Starting the container'
+docker run --rm --name todo -d -p 3000:3000 -e RAILS_MASTER_KEY=$RAILS_MASTER_KEY -v "/var/lib/docker/volumes/todo_db/_data/production.sqlite3:/code/db/production.sqlite3" docker.pkg.github.com/breckenedge/todo.breckenridge.dev/todo:latest rails s
