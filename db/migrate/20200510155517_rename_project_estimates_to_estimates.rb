4af7afa7840f3a979513ab52e4b36657fcce3b12
Aaron Breckenridge <aaronbreckenridge@gmail.com>
2020-05-17 15:22:45 -0500

    Clean up migrations

--- db/migrate/20200510155517_rename_project_estimates_to_estimates.rb
+++
@@ -1,5 +0,0 @@
-class RenameProjectEstimatesToEstimates < ActiveRecord::Migration[6.0]
-  def change
-    rename_table :project_estimates, :estimates
-  end
-end
--- db/migrate/20200515191025_add_estimates_enabled_to_project.rb
+++
@@ -1,5 +0,0 @@
-class AddEstimatesEnabledToProject < ActiveRecord::Migration[6.0]
-  def change
-    add_column :projects, :estimates_enabled, :boolean, default: false, null: false
-  end
-end
