11418ba90b938c604337b4435b8d5078ed99f870
Aaron Breckenridge <aaron@breckenridge.dev>
2021-02-20 14:23:00 -0600

    Don't deliver digests if no todos

--- app/mailers/todo_mailer.rb
+++ app/mailers/todo_mailer.rb
@@ -4,13 +4,13 @@ class TodoMailer < ApplicationMailer
     @todos = Todo.where(due_on: Date.current)
     @late_todos = Todo.late
 
-    mail(to: @user.email, subject: "Todo: Daily Digest")
+    mail(to: @user.email, subject: "Todo: Daily Digest") unless @todos.none && @late_todos.none?
   end
 
   def weekly_summary(user: User.first)
     @user = user
     @todos = Todo.completed_last_week
 
-    mail(to: @user.email, subject: "Todo: Weekly Summary")
+    mail(to: @user.email, subject: "Todo: Weekly Summary") unless @todos.none?
   end
 end
