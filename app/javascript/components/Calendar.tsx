d47e690ada002c46a0dcffa5e605520b473f5545
Aaron Breckenridge <aaron@breckenridge.dev>
2021-02-21 09:19:32 -0600

    TypeScript

--- app/javascript/components/Calendar.tsx
+++ app/javascript/components/Calendar.tsx
@@ -1,18 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import DatePicker from 'react-datepicker'
-
 import 'react-datepicker/dist/react-datepicker.css'
 
 function Calendar (props) {
-  const [value, setValue] = useState(props.value);
-
-  useEffect(() => props.onChange(value), [value])
-
-  return (
-    <>
-      <DatePicker onChange={setValue} selected={value} />
-    </>
-  )
+  return <DatePicker {...props} />
 }
 
 export default Calendar
