5eef9e33388e035acd3a313de0fa217d61c0b541
Aaron Breckenridge <aaron@breckenridge.dev>
2021-02-20 21:42:27 -0600

    Fix failing test

--- test/mailers/todo_mailer_test.rb
+++ test/mailers/todo_mailer_test.rb
@@ -3,6 +3,7 @@ require "test_helper"
 class TodoMailerTest < ActionMailer::TestCase
   test "daily_digest" do
     user = User.first!
+    Todo.create!(due_on: 1.week.ago, name: "Sample Todo")
     mail = TodoMailer.daily_digest(user: user)
     assert_equal "Todo: Daily Digest", mail.subject
     assert_equal [user.email], mail.to
