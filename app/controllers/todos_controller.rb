25c194b466e05af07dcd93b52f2240d41241513e
Aaron Breckenridge <aaron@breckenridge.dev>
2021-03-09 15:57:13 -0600

    Fix the line length violation

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -33,7 +33,9 @@ class TodosController < ApplicationController
 
     respond_to do |format|
       if @todo.save
-        format.html { redirect_to params.fetch("return_to", todo_path(@todo)), notice: "Todo was successfully created." }
+        format.html do
+          redirect_to params.fetch("return_to", todo_path(@todo)), notice: "Todo was successfully created."
+        end
       else
         format.html { render :new }
       end
