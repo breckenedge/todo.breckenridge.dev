50e28b36b5b45a7cd4ea6d5be0d0ae0d12ae5073
Aaron Breckenridge <aaron@breckenridge.dev>
2021-04-01 21:53:17 -0500

    Fix the loading indicators

--- app/javascript/pages/ProjectPage.tsx
+++ app/javascript/pages/ProjectPage.tsx
@@ -3,11 +3,11 @@ import { Link, useParams } from "react-router-dom"
 import TodosList from "components/TodosList"
 import { ProjectI, TodoI } from "interfaces"
 import { fetchProjectWithTodos } from "repos/ProjectsRepo"
+import LoadingIndicator from "components/LoadingIndicator"
 
 const ProjectPage = () => {
-  const defaultState: ProjectI = {}
   const id = useParams()["id"]
-  const [project, setProject] = useState(defaultState)
+  const [project, setProject] = useState(null)
   const [todos, setTodos] = useState([])
 
   useEffect(() => {
@@ -18,44 +18,50 @@ const ProjectPage = () => {
   }, [id])
 
   return (
-    <div>
-      <div className="two-column">
+    <>
+      {project ?
         <div>
-          <h1>{project.name}</h1>
+          <div className="two-column">
+            <div>
+              <h1>{project.name}</h1>
+            </div>
+
+            <div className="no-wrap">
+              <Link to={`/projects/${id}/edit`} className="button blue">
+                Edit
+              </Link>
+            </div>
+          </div>
+
+          <dl>
+            {project.due_date &&
+              <>
+                <dt>Due date</dt>
+                <dd>{project.due_date}</dd>
+              </>
+            }
+
+            {project.details &&
+              <>
+                <dt>Details</dt>
+                <dd>{project.details}</dd>
+              </>
+            }
+          </dl>
+
+          <div className="navigation-header">
+            <h2>Todos</h2>
+            <Link className="button teal" to={`/todos/new?project_id=${project.id}`}>
+              +
+            </Link>
+          </div>
+
+          <TodosList todos={todos} />
         </div>
-
-        <div className="no-wrap">
-          <Link to={`/projects/${id}/edit`} className="button blue">
-            Edit
-          </Link>
-        </div>
-      </div>
-
-      <dl>
-        {project.due_date &&
-          <>
-            <dt>Due date</dt>
-            <dd>{project.due_date}</dd>
-          </>
-        }
-
-        {project.details &&
-          <>
-            <dt>Details</dt>
-            <dd>{project.details}</dd>
-          </>
-        }
-      </dl>
- 
-      <div className="navigation-header">
-        <h2>Todos</h2>
-        <Link className="button teal" to={`/todos/new?project_id=${project.id}`}>
-          +
-        </Link>
-      </div>
-
-      <TodosList todos={todos} />
-    </div>
+      :
+        <LoadingIndicator/>
+      }
+    </>
   )
 }
 
--- app/javascript/pages/TodosPage.tsx
+++ app/javascript/pages/TodosPage.tsx
@@ -1,31 +1,23 @@
 import React, { useEffect, useState } from "react"
-import { TodoI } from "interfaces/TodoI"
 import { fetchTodos } from "repos/TodosRepo"
 import { Link } from "react-router-dom"
 import TodosList from "components/TodosList"
 import LoadingIndicator from "components/LoadingIndicator"
 
 const TodosPage = () => {
-  const defaultState: Array<TodoI> = []
-  const [todos, setTodos] = useState(defaultState)
-  const [isLoading, setIsLoading] = useState(true)
+  const [todos, setTodos] = useState(null)
 
   useEffect(() => {
     fetchTodos(setTodos)
-    setIsLoading(false)
   }, [])
 
   return (
     <>
-      <Link
-        to="/todos/new"
-        className="button teal button-wide">
-        New Todo
-      </Link>
+      <Link to="/todos/new" className="button teal button-wide">New Todo</Link>
 
       <br />
 
-      {isLoading ? <LoadingIndicator /> : <TodosList todos={todos} />}
+      {todos ? <TodosList todos={todos} /> : <LoadingIndicator />}
     </>
   )
 }
