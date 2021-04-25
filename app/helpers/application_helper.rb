69c89c3e7e7c634b6f63317f1ffb2c6469ad433b
Aaron Breckenridge <aaron@breckenridge.dev>
2021-04-25 16:24:35 -0500

    Remove dead code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,27 +1,2 @@
 module ApplicationHelper
-  def simple_text(text)
-    simple_format(linkify_text(text), {}, wrapper_tag: "div")
-  end
-
-  def linkify_text(text)
-    text.split("\n").map do |line|
-      line.split.map do |word|
-        if word.start_with?(%r{https?://})
-          link_to(word, word)
-        else
-          word
-        end
-      end.join(" ")
-    end.join("\n")
-  end
-
-  def due_date_badge(due_date)
-    return if due_date.blank?
-
-    days = (due_date - Date.current).to_i
-
-    tag.span(class: "badge") do
-      "#{pluralize(days.abs, "day")} #{"ago" if days.negative?}"
-    end
-  end
 end
