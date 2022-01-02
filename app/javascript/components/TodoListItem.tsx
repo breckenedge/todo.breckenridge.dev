0d782b7fee4789f3ef9cbf08ba25a2efc713469c
Aaron Breckenridge <aaron@breckenridge.dev>
2022-01-02 11:36:55 -0600

    Add ability to sort lists and filter completed

--- app/javascript/components/TodoListItem.tsx
+++ app/javascript/components/TodoListItem.tsx
@@ -4,8 +4,12 @@ import TodoCompleteToggle from "components/TodoCompleteToggle"
 import { TodoI, ProjectI } from "interfaces"
 import TodayDelta from "components/TodayDelta"
 
-const TodoListItem = ({ todo, currentProject }: { todo: TodoI, currentProject?: ProjectI }) => {
+const TodoListItem = ({ todo, currentProject, showCompleted }: { todo: TodoI, currentProject?: ProjectI, showCompleted: "t" | "f" }) => {
   const projectId = currentProject?.id || todo.project_id
+
+  if (todo.status === "complete" && showCompleted === "f") {
+    return null
+  }
 
   return (
     <div className={`todo ${todo.status}`}>
@@ -13,7 +17,7 @@ const TodoListItem = ({ todo, currentProject }: { todo: TodoI, currentProject?: 
       <Link to={projectId ? `/projects/${projectId}/todos/${todo.id}` : `/todos/${todo.id}`} className="name">
         {todo.name}
       </Link>
-      {todo.due_date && todo.status !== "complete" &&
+      {todo.due_date &&
         <div className="due-date">
           {<TodayDelta date={todo.due_date}/>}
         </div>
--- app/javascript/utilities/sortBy.ts
+++ app/javascript/utilities/sortBy.ts
@@ -1,4 +1,4 @@
-const sortBy = (key: string) => {
+const sortBy = (key: SortKey) => {
   return (objA, objB) => {
     const valA = objA[key] || ""
     const valB = objB[key] || ""
@@ -13,7 +13,7 @@ const sortBy = (key: string) => {
   }
 }
 
-export const reverseSortBy = (key: string) => {
+export const reverseSortBy = (key: SortKey) => {
   return (objA, objB) => {
     const valA = objA[key] || ""
     const valB = objB[key] || ""
@@ -25,20 +25,11 @@ export const reverseSortBy = (key: string) => {
     } else {
       return 0
     }
-  }
-}
-
-
-// Sort incomplete items before complete items
-export const sortByStatus = (objA, objB): (-1 | 0 | 1) => {
-  if (objA.status === objB.status) {
-  } else if (objA.status === "complete" && objB.status !== "complete") {
-    return 1
-  } else if (objA.status !== "complete" && objB.status === "complete") {
-    return -1
-  } else {
-    return 0
   }
 }
 
 export default sortBy
+
+export type SortDir = "asc" | "desc"
+
+export type SortKey = "created_at" | "due_date" | "name"
